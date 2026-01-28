17ecb8d70cdf1f729607322208e73f3d1d620a48
Mdetlefsen77 <Mdetlefsen77@users.noreply.github.com>
2026-01-28 14:42:13 -0300

    w4: activity form login and register routes and controllers, get and post. and views

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = new express.Router();
-const { buildLogin } = require("../controllers/accountController");
+const { buildLogin, buildRegister, registerAccount, loginAccount } = require("../controllers/accountController");
 const utilities = require("../utilities/");
 
 
 router.get("/login", utilities.handleErrors(buildLogin)
 );
+router.get("/register", utilities.handleErrors(buildRegister));
+
+router.post("/register", utilities.handleErrors(registerAccount));
+router.post("/login", utilities.handleErrors(loginAccount));
 
 module.exports = router;
